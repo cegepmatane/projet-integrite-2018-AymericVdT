eb95300460be4c9303b6f583099b7403a3c17585
Aymeric <aym1605@gmail.com>
2018-09-05 21:50:30 -0400

    Ajout des constructeurs de la classe Arme

--- src/modele/Arme.java
+++ src/modele/Arme.java
@@ -2,8 +2,45 @@ package modele;
 
 public class Arme {
 
+	protected String nom;
+	protected String type;
+	protected String famille;
+	protected String origine;
+	protected String epoque;
 	
-	
-	
+	public Arme(String nom) {
+		super();
+		this.nom = nom;
+	}
+
+	public Arme(String nom, String type) {
+		super();
+		this.nom = nom;
+		this.type = type;
+	}
+
+	public Arme(String nom, String type, String famille) {
+		super();
+		this.nom = nom;
+		this.type = type;
+		this.famille = famille;
+	}
+
+	public Arme(String nom, String type, String famille, String origine) {
+		super();
+		this.nom = nom;
+		this.type = type;
+		this.famille = famille;
+		this.origine = origine;
+	}
+
+	public Arme(String nom, String type, String famille, String origine, String epoque) {
+		super();
+		this.nom = nom;
+		this.type = type;
+		this.famille = famille;
+		this.origine = origine;
+		this.epoque = epoque;
+	}
 	
 }
