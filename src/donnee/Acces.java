c0fc920fbd1f4f42a420f975f99f2c9d5b3f58ac
Aymeric <aym1605@gmail.com>
2018-09-20 21:53:51 -0400

    Creation de la classe acces et changement de basededonnees

---
+++ src/donnee/Acces.java
@@ -0,0 +1,11 @@
+package donnee;
+
+public class Acces {
+	
+	static final String BASEDEDONNEES_DRIVER = "org.postgresql.Driver";
+	static final String BASEDEDONNEES_URL = "jdbc:postgresql://localhost:5432/collectionArmes";
+	static final String BASEDEDONNEES_USAGER = "postgres";
+	static final String BASEDEDONNEES_MOTDEPASSE = "test";	
+	
+
+}
